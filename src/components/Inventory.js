8bf7fb103d2a424b948ca4c89623d582c8998a78
adarsh1997shah <adarshshah01@gmail.com>
2021-01-17 20:26:58 +0530

    minor css changes

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -7,176 +7,172 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import { base } from '../base';
 
-
-
 class Inventory extends React.Component {
+  static propTypes = {
+    fishes: PropTypes.shape({
+      image: PropTypes.string,
+      desc: PropTypes.string,
+      name: PropTypes.string,
+      price: PropTypes.number,
+      status: PropTypes.string,
+    }),
+    index: PropTypes.string,
+    editFish: PropTypes.func,
+    deleteFish: PropTypes.func,
+    loadSampleFish: PropTypes.func,
+    addFish: PropTypes.func,
+    storeId: PropTypes.string,
+  };
 
-	static propTypes = {
-		fishes: PropTypes.shape( {
-			image: PropTypes.string,
-			desc: PropTypes.string,
-			name: PropTypes.string,
-			price: PropTypes.number,
-			status: PropTypes.string
-		} ),
-		index: PropTypes.string,
-		editFish: PropTypes.func,
-		deleteFish: PropTypes.func,
-		loadSampleFish: PropTypes.func,
-		addFish: PropTypes.func,
-		storeId: PropTypes.string,
-	}
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      isLoading: true,
+      uid: null,
+      owner: null,
+      name: null,
+    };
+  }
 
-	constructor( props ) {
-		super( props );
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        //firebase.auth().signOut()
+        this.authHandler({ user });
+      } else {
+        this.setState({ isLoading: false });
+      }
+    });
+  }
 
-		this.state = {
-			isLoading: true,
-			uid: null,
-			owner: null,
-			name: null,
-		}
-	}
+  // Handle the authenticated user.
+  authHandler = response => {
+    let userDetails = {};
+    this.setState({ isLoading: true });
 
+    base
+      .fetch(this.props.storeId, {
+        context: this,
+      })
+      .then(data => {
+        // Check if the owner exists.
+        if (!data.owner) {
+          userDetails = {
+            uid: response.user.uid,
+            email: response.user.email,
+            name: response.user.displayName,
+          };
 
-	componentDidMount() {
+          base.post(`${this.props.storeId}/owner`, {
+            data: { ...userDetails },
+          });
+        }
 
-		firebase.auth().onAuthStateChanged( ( user ) => {
-			if( user ) {
-				//firebase.auth().signOut()
-				this.authHandler( { user } );
-			}else {
-				this.setState( { isLoading: false } );
-			}
-		} );
-	}
+        this.setState({
+          isLoading: false,
+          uid: response.user.uid,
+          owner: data.owner ? data.owner.uid : response.user.uid,
+          name: response.user.displayName,
+        });
+      })
+      .catch(err => {
+        console.log('Opps you have an error ' + err.message);
+      });
+  };
 
+  // Authenticate the user.
+  authenticate = loginMethod => {
+    const provider = new firebase.auth[`${loginMethod}AuthProvider`]();
 
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(this.authHandler)
+      .catch(err => {
+        console.log('Opps you got some error! ' + err.message);
+      });
+  };
 
-	// Handle the authenticated user.
-	authHandler = ( response ) => {
+  // Logging the user out.
+  logout = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        console.log('sign out successfull');
 
-		let userDetails = {};
-		this.setState( { isLoading: true } );
+        this.setState({ uid: null });
+      })
+      .catch(err => {
+        console.log('Opps problem with signing out', err.message);
+      });
+  };
 
-		base.fetch( this.props.storeId, {
-			context: this,
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <React.Fragment>
+          <div className="inventory">
+            <h2>Inventory</h2>
+          </div>
+        </React.Fragment>
+      );
+    } else {
+      if (!this.state.uid) {
+        return (
+          <React.Fragment>
+            <Login authenticate={this.authenticate} />
+          </React.Fragment>
+        );
+      }
 
-		} ).then( ( data ) => {
+      if (this.state.uid !== this.state.owner) {
+        return (
+          <React.Fragment>
+            <div>
+              <button onClick={this.logout} style={{ marginBottom: '10px' }}>
+                Log Out
+              </button>
+              <div>Sorry you are not the owner</div>
+            </div>
+          </React.Fragment>
+        );
+      }
 
-			// Check if the owner exists.
-			if( !data.owner ) {
+      return (
+        <React.Fragment>
+          <div className="inventory">
+            <h2>Inventory</h2>
 
-				userDetails = {
-					uid: response.user.uid,
-					email: response.user.email,
-					name: response.user.displayName,
-				}
+            <h4>
+              <center>Welcome, {this.state.name}</center>
+            </h4>
 
-				base.post( `${this.props.storeId}/owner`, {
-					data: {...userDetails}
-				} );
-			}
+            <button onClick={this.logout} style={{ marginBottom: '10px' }}>
+              Log Out
+            </button>
 
-			this.setState( {
-				isLoading: false,
-				uid: response.user.uid,
-				owner: data.owner? data.owner.uid : response.user.uid,
-				name: response.user.displayName,
-			} );
-
-		} ).catch( ( err ) => {
-			console.log( 'Opps you have an error ' + err.message);
-		} )
-	}
-
-
-
-	// Authenticate the user.
-	authenticate = ( loginMethod ) => {
-		const provider = new firebase.auth[`${loginMethod}AuthProvider`]();
-
-		firebase.auth().signInWithPopup( provider )
-			.then( this.authHandler )
-			.catch( ( err ) => {
-			console.log( 'Opps you got some error! ' + err.message );
-		} );
-	}
-
-
-	// Logging the user out.
-	logout = () => {
-		firebase.auth().signOut()
-			.then( () => {
-				console.log( 'sign out successfull' );
-
-				this.setState( { uid: null } );
-			} )
-			.catch( ( err ) => {
-				console.log( 'Opps problem with signing out', err.message );
-			} )
-	}
-
-
-	render() {
-		if( this.state.isLoading ) {
-			return (
-				<React.Fragment>
-					<div className="inventory">
-						<h2>Inventory</h2>
-					</div>
-				</React.Fragment>
-			);
-	
-		} else {
-			if( !this.state.uid ) {
-				return (
-					<React.Fragment>
-						<Login authenticate={this.authenticate} />
-					</React.Fragment>	
-				);
-			}
-
-
-			if( this.state.uid !== this.state.owner ) {
-				return (
-					<React.Fragment>
-						<div>
-							<button onClick={this.logout}>Log Out</button>
-							<div>Sorry you are not the owner</div>
-						</div>
-					</React.Fragment>
-				);
-			}
-
-			return(
-				<React.Fragment>
-					<div className="inventory">
-						<h2>Inventory</h2>
-
-						<h4><center>Welcome, {this.state.name}</center></h4>
-
-						<button onClick={this.logout}>Log Out</button>
-					
-						{
-							Object.keys( this.props.fishes ).map( ( fish ) => {
-								return <EditFishForm
-											key={fish}
-											index={fish}
-											details={this.props.fishes[fish]}
-											editFish={this.props.editFish}
-											deleteFish={this.props.deleteFish}
-										/>
-							} )
-						}
-						<AddFishForm addFish={this.props.addFish} />
-						<button onClick={this.props.loadSampleFish}>Load Sample Fishes</button>
-					</div>
-					</React.Fragment>
-			);
-		}
-	}
+            {Object.keys(this.props.fishes).map(fish => {
+              return (
+                <EditFishForm
+                  key={fish}
+                  index={fish}
+                  details={this.props.fishes[fish]}
+                  editFish={this.props.editFish}
+                  deleteFish={this.props.deleteFish}
+                />
+              );
+            })}
+            <AddFishForm addFish={this.props.addFish} />
+            <button onClick={this.props.loadSampleFish}>
+              Load Sample Fishes
+            </button>
+          </div>
+        </React.Fragment>
+      );
+    }
+  }
 }
 
 export default Inventory;
--- src/components/Order.js
+++ src/components/Order.js
@@ -3,100 +3,94 @@ import PropTypes from 'prop-types';
 import { formatPrice } from '../helpers';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
-
-
 class Order extends React.Component {
+  static propTypes = {
+    fishes: PropTypes.shape({
+      image: PropTypes.string,
+      desc: PropTypes.string,
+      name: PropTypes.string,
+      price: PropTypes.number,
+      status: PropTypes.string,
+    }),
+    orders: PropTypes.object,
+    deleteOrder: PropTypes.func,
+  };
 
-	static propTypes = {
-		fishes: PropTypes.shape( {
-			image: PropTypes.string,
-			desc: PropTypes.string,
-			name: PropTypes.string,
-			price: PropTypes.number,
-			status: PropTypes.string
-		} ),
-		orders: PropTypes.object,
-		deleteOrder: PropTypes.func
-	}
+  // To calculate the total price.
+  calcTotalPrice = () => {
+    const total = Object.keys(this.props.orders).reduce((sum, order) => {
+      const fish = this.props.fishes[order];
+      const count = this.props.orders[order];
 
+      // If there is fish with that index and is available;
+      const isavailable = fish && fish.status === 'available' ? true : false;
 
-	// To calculate the total price.
-	calcTotalPrice = () => {
-		const total = Object.keys( this.props.orders ).reduce( (sum, order ) => {
-			const fish = this.props.fishes[order];
-			const count = this.props.orders[order];
+      if (isavailable) {
+        return sum + count * fish.price;
+      }
+      return sum;
+    }, 0);
 
-			// If there is fish with that index and is available;
-			const isavailable = fish && fish.status === 'available'? true : false;
+    return total;
+  };
 
-			if( isavailable ) {
-				return sum + (count * fish.price);
-			}
-			return sum;
-		}, 0 );
+  // Render list of order.
+  renderOrder = order => {
+    const fish = this.props.fishes[order];
+    const count = this.props.orders[order];
 
-		return total;
-	}
+    // If there is fish with that index and is available;
+    const isavailable = fish && fish.status === 'available' ? true : false;
 
+    // Make sure the fish is available.
+    if (!fish) return null;
 
+    if (!isavailable) {
+      return (
+        <li key={order}>
+          Sorry{' '}
+          {fish
+            ? fish.name + 'has been sold out'
+            : 'fish is no longer availble'}
+        </li>
+      );
+    }
 
-	// Render list of order.
-	renderOrder = ( order ) => {
-		const fish = this.props.fishes[order];
-		const count = this.props.orders[order];
+    return (
+      <CSSTransition timeout={300} classNames="order" key={order}>
+        <li key={order}>
+          <TransitionGroup component="span" className="count">
+            <CSSTransition timeout={300} classNames="count" key={count}>
+              <span>{`${count} lbs`}</span>
+            </CSSTransition>
+          </TransitionGroup>
+          <span>
+            {fish.name} {formatPrice(fish.price * count)}
+          </span>
+          <button onClick={() => this.props.deleteOrder(order)}>&times;</button>
+        </li>
+      </CSSTransition>
+    );
+  };
 
-		// If there is fish with that index and is available;
-		const isavailable = fish && fish.status === 'available'? true : false;
+  render() {
+    return (
+      <React.Fragment>
+        <div className="order">
+          <h2>Order</h2>
 
-		// Make sure the fish is available.
-		if( !fish ) return null;
+          <TransitionGroup component="ul" className="order">
+            {Object.keys(this.props.orders).map(this.renderOrder)}
+          </TransitionGroup>
 
-		if( !isavailable ) {
-			return <li key={order}>Sorry {fish ? fish.name + 'has been sold out' : 'fish is no longer availble'}</li>
-		}
-
-		return (
-			<CSSTransition
-				timeout={300}
-				classNames="order"
-				key={order}
-			>
-				<li key={order}>
-					<span>
-						<TransitionGroup component="span" className="count" >
-							<CSSTransition timeout={300} classNames="count" key={count} >
-								<span>{count}</span>
-							</CSSTransition>
-						</TransitionGroup>
-
-						lbs {fish.name} {formatPrice( fish.price * count )}
-						<button onClick={ () => this.props.deleteOrder( order ) } >&times;</button>
-					</span>
-				</li>
-			</CSSTransition>
-		);
-	}
-
-
-	render() {
-		return(
-			<React.Fragment>
-				<div className="order">
-					<h2>Order</h2>
-
-					<TransitionGroup component="ul" className="order">
-						{
-							Object.keys( this.props.orders ).map( this.renderOrder )
-						}
-					</TransitionGroup>
-
-					<div className="total">
-						<span>Total:</span><strong>{formatPrice(this.calcTotalPrice())}</strong>
-					</div>
-				</div>
-			</React.Fragment>	
-		);
-	}
+          <div className="total">
+            <span>Total:</span>
+            <strong>{formatPrice(this.calcTotalPrice())}</strong>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Order;
